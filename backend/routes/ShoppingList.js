4a33473f2b3dca1845cd847fe9be33c90e3b0d4c
Kris Bautista <krisb09@gmail.com>
2022-03-23 00:24:48 -0400

    first commit

---
+++ backend/routes/ShoppingList.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/read', (req, res) => {
+
+})
+
+router.post('/add', (req, res) => {
+
+})
+
+router.put("/add", (req, res) => {
+
+});
+
+router.delete("/add", (req, res) => {
+    
+});
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+/* Middleware */
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+/* Important Routes */
+const ShoppingList = require('../backend/routes/ShoppingList')
+
+app.use('/shoppingList', ShoppingList)
+app.use('/read', ShoppingList)
+
+/* DB CONNECTION */
+mongoose.connect('mongodb://localhost:27017/mern-ShoppingList')
+
+/* Listening to Server */
+app.listen(7001, () => console.log('Listening on port 7001...'));
