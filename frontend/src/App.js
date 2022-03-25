bbdce187fba79243018fecb4e4a59dfad5e80674
Kris Bautista <krisb09@gmail.com>
2022-03-25 03:50:00 -0400

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import useForm from "./useForm";
 import "./App.css";
 
 const App = () => {
-  const { values, handleOnChange } = useForm({item: 'carrots'});
+  const { values, handleOnChange } = useForm();
+  const [ dataResults, setDataResults ] = useState([]);
 
   const getItem = async () => {
     const results = await Axios({
@@ -12,20 +13,83 @@ const App = () => {
       url: 'http://localhost:7001/shoppingList/read'
     })
 
-    console.log('results: ', results)
+    setDataResults(results.data)
   }
 
-  const onSubmit = () => {
-    console.log('test');
+  const addItem = async () => {
+    const results = await Axios({
+      method: 'POST',
+      url: 'http://localhost:7001/shoppingList/create',
+      data: {
+        ...values
+      }
+    })
+
+    return results.data.status
+  };
+
+  const deleteItem = async (_id) => {
+    const results = await Axios({
+      method: "DELETE",
+      url: "http://localhost:7001/shoppingList/delete",
+      data: {
+        _id,
+      },
+    });
+
+    if (results.data.status) {
+      alert('Successfully Deleted!')
+      await getItem();
+    } else {
+      alert("Failed!");
+    }
+
+    return results.data.status;
+  };
+
+  const [state, setState] = useState({})
+
+  const updateItem = async () => {
+    const { _id, item, quantity } = state
+
+    const results = await Axios({
+      method: "PUT",
+      url: "http://localhost:7001/shoppingList/update",
+      data: {
+        _id,
+        set: {
+          item,
+          quantity
+        }
+      },
+    });
+
+    if (results.data.status) {
+      alert('List Updated!')
+      await getItem();
+    } else {
+      alert('List Not Updated!')
+    }
+
+    return results.data.status;
   }
 
-  console.log('values: ', values);
+  const onSubmit = async () => {
+    if (await addItem()) {
+      alert('Item added to list!')
+    } else {
+      alert('Item not added to list!')
+    }
+  }
+
+  //console.log('values: ', values);
 
   useEffect(() => {
     getItem()
 
     return () => getItem
   }, [])
+
 
   return (
     <div className="App">
@@ -48,15 +112,10 @@ const App = () => {
         />
         <input type="submit" className="add-item" onClick={onSubmit} />
 
-        <table className="table">
-          
-        </table>
+
       </div>
     </div>
-  );
-
-  
-    
+  );   
 }
 
 export default App;
